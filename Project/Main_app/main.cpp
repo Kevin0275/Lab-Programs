9186c8d230da877057558bf5f04bb5d07cee0687
Kevin0275 <kevin0275000@gmail.com>
2025-07-08 15:18:09 +0800

    This is the new version: You can obtain both Distance and the Amplitude. Also, I have added a new file for you to obtain the device index for microphone.

--- Project/Main_app/main.cpp
+++ Project/Main_app/main.cpp
@@ -1,3 +1,53 @@
+#include <portaudio.h>
+#include <vector>
+#include <cmath>
+
+#define SAMPLE_RATE 44100
+#define FRAMES_PER_BUFFER 256
+
+float getMicRMS() {
+    PaStream *stream;
+    PaStreamParameters inputParameters;
+    std::vector<float> buffer(FRAMES_PER_BUFFER);
+    float rms = 0;
+
+    if (Pa_Initialize() != paNoError)
+        return -1;
+
+    inputParameters.device = 13; // ← your Conexant SmartAudio HD
+    if (inputParameters.device == paNoDevice) {
+        Pa_Terminate();
+        return -1;
+    }
+
+    inputParameters.channelCount = 1; // or 2 if you want stereo input
+    inputParameters.sampleFormat = paFloat32;
+    inputParameters.suggestedLatency = Pa_GetDeviceInfo(inputParameters.device)->defaultLowInputLatency;
+    inputParameters.hostApiSpecificStreamInfo = nullptr;
+
+    if (Pa_OpenStream(&stream, &inputParameters, nullptr,
+                      SAMPLE_RATE, FRAMES_PER_BUFFER, paClipOff,
+                      nullptr, nullptr) != paNoError) {
+        Pa_Terminate();
+        return -1;
+    }
+
+    Pa_StartStream(stream);
+    Pa_ReadStream(stream, buffer.data(), FRAMES_PER_BUFFER);
+    Pa_StopStream(stream);
+    Pa_CloseStream(stream);
+    Pa_Terminate();
+
+    float sum = 0;
+    for (float s : buffer)
+        sum += s * s;
+
+    rms = std::sqrt(sum / buffer.size());
+    return rms;
+}
+
+ 
+
 #include <QCoreApplication>
 #include <QSerialPort>
 #include <QTextStream>
@@ -22,9 +72,12 @@ int main(int argc, char *argv[])
     }
 
     QObject::connect(&serial, &QSerialPort::readyRead, [&]() {
-        QByteArray line = serial.readLine();
-        QTextStream(stdout) << "Distance: " << line;
+    QByteArray line = serial.readLine();
+    float micRMS = getMicRMS();
+    QTextStream(stdout) << "Distance: " << line.trimmed()
+                        << " | Mic RMS: " << micRMS << "\n";
     });
+
 
     return a.exec();
 }
